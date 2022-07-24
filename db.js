4cdea768789ef36f4d1ca58362473b0bb49a366c
Achilleaus <sejao23@gmail.com>
2022-07-24 16:22:25 +0530

    first commit

---
+++ db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+// URL to connect to the database 
+// const mongoURI = "mongodb+srv://jenil:jenil@demo.7fmbb.mongodb.net/inotebook?authSource=admin&replicaSet=atlas-lugv9y-shard-0&w=majority&readPreference=primary&retryWrites=true&ssl=true"
+const mongoURI = "mongodb+srv://Sharwil:Sharwilm2@sharwil.n07vq.mongodb.net/test"
+// mongodb+srv://jenil:jenil@demo.7fmbb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+
+// Connect to the database by creating function of connectToMongo()
+const connectToMongo = () =>{
+    mongoose.connect(mongoURI, ()=>{
+        console.log("Connected to Mongo Sucessfully")
+    })
+}
+
+module.exports = connectToMongo;
---
+++ index.js
@@ -0,0 +1,33 @@
+const connectToMongo = require('./db'); // Get the acces from ('./db') to access the connectToMongo() function that is created in that component
+const express = require('express')
+require("dotenv").config();
+var cors = require('cors')
+
+// Call the connectToMongo() function in main file to access the database by the backend
+connectToMongo();
+const app = express()
+// I can change the port anytime so I can't get confuse in different files when I run them 
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+// To send the JSON body request to database and read it in database
+app.use(express.json())
+
+// Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
+// stackoverflow person's code
+// const apiRouter = express.Router();
+// const authRouter = express.Router();
+// const notesRouter = express.Router();
+// authRouter.use("/auth", require('./routes/auth'));
+// notesRouter.use("/notes", require('./routes/notes'));
+// apiRouter.use(authRouter);
+// apiRouter.use(notesRouter);
+// app.use("/api", apiRouter);
+
+// Validation that the port is running
+app.listen(port, () => {
+  console.log(`iNotebook backend listening on port ${port}`)
+})
